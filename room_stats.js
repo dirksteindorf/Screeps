303b40413cc989551acd728dd7214c8c339f422e
Dirk Steindorf <dirk.steindorf@gmail.com>
2016-07-13 16:27:20 +0200

    small correction in room_stats

--- room_stats.js
+++ room_stats.js
@@ -5,11 +5,11 @@ module.exports = {
 
             //------------------------------------------------------------------
             // general useful information
-            room.memory.maxHarvesters = 0;
 
             //------------------------------------------------------------------
             // sources
             if(!room.memory.sources){
+                room.memory.maxHarvesters = 0;
                 room.memory.sources = [];
                 var sources = room.find(FIND_SOURCES);
 
@@ -17,7 +17,7 @@ module.exports = {
                     room.memory.sources[s] = {};
                     room.memory.sources[s].id = sources[s].id;
                     room.memory.sources[s].harvesters = 0;
-                    room.memory.sources[s].maxHarvesters = 4; 
+                    room.memory.sources[s].maxHarvesters = 4;
 
                     room.memory.maxHarvesters += room.memory.sources[s].maxHarvesters;
                 }
