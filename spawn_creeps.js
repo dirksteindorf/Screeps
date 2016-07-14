6db9318a801599530a1407e610ecab5ad66e4663
Dirk Steindorf <dirk.steindorf@gmail.com>
2016-07-14 20:20:00 +0200

    spawn more upgraders

--- spawn_creeps.js
+++ spawn_creeps.js
@@ -46,10 +46,9 @@ module.exports = {
         }
 
         // build upgraders when at least one miner is present
-        else if(Memory.creepCount.upgrader < 3){
+        else if(Memory.creepCount.upgrader < 4){
             if(Game.creeps.Miner1 && Game.spawns.Spawn1.room.controller.level >= 3 && Game.spawns.Spawn1.room.energyAvailable >= 800){
                 Game.spawns.Spawn1.createCreep([WORK, WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE], "upgrader"+Game.time, {role: "upgrader"});
-                console.log("HERE");
             }
             else if(Game.creeps.Miner1 && Game.spawns.Spawn1.room.controller.level >= 2 && Game.spawns.Spawn1.room.energyAvailable >= 350){
                 Game.spawns.Spawn1.createCreep([WORK, WORK, MOVE, CARRY, MOVE], "upgrader"+Game.time, {role: "upgrader"});
