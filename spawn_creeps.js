7ef42672da279e60491e80a441d85213090233d9
Dirk Steindorf <dirk.steindorf@gmail.com>
2016-07-14 20:16:58 +0200

    added spawning of stronger harvesters

--- spawn_creeps.js
+++ spawn_creeps.js
@@ -8,6 +8,9 @@ module.exports = {
 
         // first harvesters as a foundation for the chain
         if(Memory.creepCount.harvester < 2 || Memory.creepCount.harvester < Game.rooms['W17S41'].memory.maxHarvesters){
+            if(Game.spawns.Spawn1.room.energyAvailable >= 800){
+                Game.spawns.Spawn1.createCreep([WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE], "harvester"+Game.time, {role: "harvester", task: "harvesting"});
+            }
             if(Game.spawns.Spawn1.room.controller.level >= 3 && Game.spawns.Spawn1.room.energyAvailable >= 500){
                 Game.spawns.Spawn1.createCreep([WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE], "harvester"+Game.time, {role: "harvester", task: "harvesting"});
             }
